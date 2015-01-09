3ec75139d6559df4d5fb7a03912c1ffd44dc3647
hrobeers <hansrobeers@gmail.com>
2015-01-10 00:37:15 +0100

    Path editable by PointHandles

--- js/patheditor.js
+++ js/patheditor.js
@@ -7,10 +7,10 @@ function initPathEditor(paper, path, elementID){
         paper.clear();
 
     // Create the path
-    var p = paper.path(path);//.attr({stroke: color || Raphael.getColor(), "stroke-width": 4, "stroke-linecap": "round"});
+    var p = new EditableCurve(paper, path);
     var bBox = p.getBBox();
     paper.setViewBox(-5, -(bBox.height+5), paper.width + 10, paper.height + 10);
-    createPointHandles(paper, p);
+    p.initPointHandles();
 
     // Decorate
     paper.rect(bBox.x, bBox.y, bBox.width, bBox.height);
@@ -18,61 +18,90 @@ function initPathEditor(paper, path, elementID){
     return paper;
 }
 
-function makeEditable(element){
-    element.followingPnts = [];
+function EditableCurve(paper, pathArray){
+    this.pathArray = pathArray;
+    this.path = paper.path(pathArray);
+    this.paper = paper;
 
-    element.update = function(x, y){
+    this.getBBox = function() { return this.path.getBBox(); }
+
+    this.initPointHandles = function (){
+        var paper = this.paper;
+        var curve = this;
+        var handlePath = [];
+        var prevSet;
+
+        var i=0;
+        while(i<this.pathArray.length){
+            var set;
+            switch (this.pathArray[i].length)
+            {
+            case 3:
+                set = [ new PointHandle(paper, curve, i, 1) ];
+//                handlePath.push(["M", e[1], e[2]]);
+                break;
+
+            case 7:
+                prevSet.push( new PointHandle(paper, curve, i, 1) );
+                prevSet[prevSet.length-2].followingPnts().push(prevSet[prevSet.length-1]);
+                set = [
+                    new PointHandle(paper, curve, i, 2),
+                    new PointHandle(paper, curve, i, 3)
+                ];
+                set[1].followingPnts().push(set[0]);
+//                handlePath.push(["L", e[1], e[2]]);
+//                handlePath.push(["M", e[3], e[4]]);
+//                handlePath.push(["L", e[5], e[6]]);
+                break;
+            }
+            prevSet = set;
+            i++;
+        }
+//        paper.path(handlePath).attr({stroke: "#000", "stroke-dasharray": "- "});
+    }
+}
+
+function PointHandle(paper, curve, elementIdx, pntIdx){
+    var isCubic = (curve.pathArray[elementIdx][0] === "C");
+
+    //
+    // Init the circle
+    //
+    var discattr = (isCubic && pntIdx < 3) ?
+                    {fill: "#aaa", stroke: "#000"}: // ControlPoint
+                    {fill: "#fff", stroke: "#000"}; // PathPoint
+    this.circle = paper.circle(curve.pathArray[elementIdx][pntIdx*2-1],
+                                    curve.pathArray[elementIdx][pntIdx*2], 5).attr(discattr);
+
+    this.circle.followingPnts = [];
+
+    this.circle.update = function(x, y){
         var X = this.attr("cx") + x,
             Y = this.attr("cy") + y;
         this.attr({cx: X, cy: Y});
-        this.followingPnts.map( function(e){
-           e.update(x, y);
+        this.followingPnts.forEach( function(e){
+           e.circle.update(x, y);
         });
+        curve.pathArray[elementIdx][pntIdx*2-1] = X;
+        curve.pathArray[elementIdx][pntIdx*2] = Y;
+        curve.path.attr({path: curve.pathArray});
+    };
+
+    this.circle.move = function(dx, dy) {
+        this.update(dx - (this.dx || 0), dy - (this.dy || 0));
+        this.dx = dx;
+        this.dy = dy;
     }
+    this.circle.up = function() {
+        this.dx = this.dy = 0;
+    }
+    this.circle.drag(this.circle.move, this.circle.up);
 
-    element.drag(move, up);
 
-    return element;
-}
-function move(dx, dy) {
-    this.update(dx - (this.dx || 0), dy - (this.dy || 0));
-    this.dx = dx;
-    this.dy = dy;
-}
-function up() {
-    this.dx = this.dy = 0;
-}
-
-function createPointHandles(paper, path){
-    var discattr = {fill: "#fff", stroke: "#000"}; //stroke: "none"};
-    var discattr_cpnt = {fill: "#aaa", stroke: "#000"}; //stroke: "none"};
-    var handlePath = [];
-    var prevSet;
-    path.realPath.map( function(e){
-        var set;
-        switch (e.length)
-        {
-        case 3:
-            set = [ makeEditable(paper.circle(e[1], e[2], 5).attr(discattr)) ];
-            handlePath.push(["M", e[1], e[2]]);
-            break;
-
-        case 7:
-            prevSet.push( makeEditable(paper.circle(e[1], e[2], 5).attr(discattr_cpnt)) );
-            prevSet[prevSet.length-2].followingPnts.push(prevSet[prevSet.length-1]);
-            set = [
-                makeEditable(paper.circle(e[3], e[4], 5).attr(discattr_cpnt)),
-                makeEditable(paper.circle(e[5], e[6], 5).attr(discattr))
-            ];
-            set[1].followingPnts.push(set[0]);
-            handlePath.push(["L", e[1], e[2]]);
-            handlePath.push(["M", e[3], e[4]]);
-            handlePath.push(["L", e[5], e[6]]);
-            break;
-        }
-        prevSet = set;
-    })
-    paper.path(handlePath).attr({stroke: "#000", "stroke-dasharray": "- "});
+    //
+    // PointHandle methods
+    //
+    this.followingPnts = function() { return this.circle.followingPnts; }
 }
 
 function isPaper(obj){
