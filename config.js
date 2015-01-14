806815c393700f2a82d03810811d2a4272f73823
hrobeers <hansrobeers@gmail.com>
2015-01-14 18:46:12 +0100

    Default foil pre-loaded

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var config={
     wsUrl: "http://localhost:8080",
-    filesUrl: "files"
+    filesUrl: "files",
+    defaultFoil: JSON.parse('{"layerCount":6,"outline":{"area":0.007554,"height":0.1,"path":[["M",0,0],["C",32.191,-63.065,140.799,-227.156,269.501,-228.969],["C",296.16,-229.345,336.99,-220.89,340.61,-194.48],["C",342.965,-177.30,269.21,-156.231,247.10,-124.084],["C",199.757,-55.2408,226.88,-13,232.88,0]]},"profile":{"symmetry":"Symmetric","thickness":0.01,"thicknessRatio":1,"topProfile":[["M",0,0],["C",0,0,0,-35,90,-35],["C",135,-35,300,0,300,0]]},"thickness":{"topProfile":[["M",0,-30],["C",0,-30,300,-30,300,0]]}}')
 };
--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
-
-$( "#downlBtn" ).attr("disabled", true);
+//$(document).ready(function(){
 
 var currentFileName;
-var foil;
+var currentFoil;
 
 var outsideEditor;
 var insideEditor;
 var profileEditor;
 var thicknessEditor;
+
+function loadFoil(foil) {
+    outsideEditor = initPathEditor(outsideEditor, foil.outline.path, "oImg", true);
+    insideEditor = initPathEditor(insideEditor, foil.outline.path, "iImg", false);
+    profileEditor = initPathEditor(profileEditor, foil.profile.topProfile, "pImg", true);
+    thicknessEditor = initPathEditor(thicknessEditor, foil.thickness.topProfile, "tImg", true);
+    currentFoil = foil;
+}
 
 function loadFile(evt) {
   //Retrieve the first (and only!) File from the FileList object
@@ -16,22 +22,15 @@ function loadFile(evt) {
   currentFileName = f.name;
 
   readFile(f, function(content){
-    foil = JSON.parse(content).foil;
-
-    outsideEditor = initPathEditor(outsideEditor, foil.outline.path, "oImg", true);
-    insideEditor = initPathEditor(insideEditor, foil.outline.path, "iImg", false);
-    profileEditor = initPathEditor(profileEditor, foil.profile.topProfile, "pImg", true);
-    thicknessEditor = initPathEditor(thicknessEditor, foil.thickness.topProfile, "tImg", true);
+    loadFoil(JSON.parse(content).foil);
   });
-
-  $( "#downlBtn" ).attr("disabled", false);
 }
 
 function download() {
   if (currentFileName === "")
   { /* TODO handle error*/ }
 
-  ffserverStlCall(JSON.stringify(foil), currentFileName, function(data){
+  ffserverStlCall(JSON.stringify(currentFoil), currentFileName, function(data){
     var stlPath = config.filesUrl + data;
     $( "#permaLink" ).text(stlPath);
     window.location.href = stlPath;
@@ -40,5 +39,7 @@ function download() {
 }
 
 document.getElementById('fileinput').addEventListener('change', loadFile, false);
+loadFoil(config.defaultFoil);
+$( "#downlBtn" ).attr("disabled", false);
 
-});
+//});
